71e94a3f0a0504394345fe36e5b72af872f34120
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-24 11:10:12 +0800

    [update] challenge 5-8

--- app.js
+++ app.js
@@ -14,17 +14,27 @@ app.set('view engine', 'ejs');
 app.use(exoress.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+
 app.get("/", function(req, res) {
   res.render("home", {startingContent: homeStartingContent});
 });
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactContent: contactContent});
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
-
-
-
-
+app.post("/compose", function(req, res) {
+  console.log("req.body.postTitle");
+});
 
 
 
